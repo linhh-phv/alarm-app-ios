d2435233a750cff54be25befc8061991188b5f70
linhh-phv <linhpv@hasaki.vn>
2024-12-14 23:11:25 +0700

    Initial Commit

---
+++ AlarmApp/AlarmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmAppApp.swift
+//  AlarmApp
+//
+//  Created by Phạm Linh on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AlarmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
